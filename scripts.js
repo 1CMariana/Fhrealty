da406517c430411cc6fe2963f4c34df8c94e1487
1CMariana <mclaro0301@gmail.com>
2024-07-02 10:06:53 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+// Example: Toggle mobile menu
+const toggleMenu = () => {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('show');
+};
+
+const menuButton = document.querySelector('.menu-button');
+menuButton.addEventListener('click', toggleMenu);
